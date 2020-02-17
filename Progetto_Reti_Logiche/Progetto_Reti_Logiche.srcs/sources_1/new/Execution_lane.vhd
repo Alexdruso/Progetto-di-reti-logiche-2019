4bd59ff8d9f76e0fd23c857523e8dc3b33781cee
Alessandro <alessandro1.sanvito@mail.polimi.it>
2020-02-17 18:06:48 +0100

    add execution lane

--- Progetto_Reti_Logiche/Progetto_Reti_Logiche.srcs/sources_1/new/Execution_lane.vhd
+++ Progetto_Reti_Logiche/Progetto_Reti_Logiche.srcs/sources_1/new/Execution_lane.vhd
@@ -21,6 +21,7 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -33,20 +34,28 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity Execution_lane is
 
-    generic(
-        N : integer := 7
-        );
-    
      port(
-        wz_vector,data_in : in std_logic_vector(N downto 0);
-        coded_value : out std_logic_vector(N downto 0);
-        valid : out std_logic
+        wz_vector,data_in : in std_logic_vector(7 downto 0);
+        valid : out std_logic;
+        one_hot_code : out std_logic_vector(3 downto 0)
         );  
 end Execution_lane;
 
+
 architecture Dataflow of Execution_lane is
+
+component Decoder 
+    port(
+        addr: in std_logic_vector(1 downto 0);
+        out1: out std_logic_vector(3 downto 0)
+   );
+end component; 
+
+signal diff : std_logic_vector(7 downto 0);
 
 begin
 
-
+diff <= data_in+wz_vector+1;
+D: Decoder port map (addr=>diff(1 downto 0),out1=>one_hot_code);
+valid <= not( diff(7) or diff(6) or diff(5) or diff(4) or diff(3) or diff(2) );
 end Dataflow;
