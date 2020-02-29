be8665cff4857b7d690810c7d866c88b8ba9fa68
Andrea Riva <7723068+a-riva@users.noreply.github.com>
2020-02-29 18:46:42 +0100

    Revert "fix: use adder with 8 bits"
    This reverts commit ccfbd931a849bcd5566db4972e289cf3fa9d1bab.

--- Progetto_Reti_Logiche/Progetto_Reti_Logiche.srcs/sources_1/new/project_reti_logiche.vhd
+++ Progetto_Reti_Logiche/Progetto_Reti_Logiche.srcs/sources_1/new/project_reti_logiche.vhd
@@ -1,144 +1,216 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-use ieee.std_logic_unsigned.all;
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 20.02.2020 21:43:51
+-- Design Name: 
+-- Module Name: project_reti_logiche - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
 
-entity project_tb is
-end project_tb;
 
---da_ale
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
 
-architecture projecttb of project_tb is
-constant c_CLOCK_PERIOD		: time := 100 ns;
-signal   tb_done		: std_logic;
-signal   mem_address		: std_logic_vector (15 downto 0) := (others => '0');
-signal   tb_rst	                : std_logic := '0';
-signal   tb_start		: std_logic := '0';
-signal   tb_clk		        : std_logic := '0';
-signal   mem_o_data,mem_i_data	: std_logic_vector (7 downto 0);
-signal   enable_wire  		: std_logic;
-signal   mem_we		        : std_logic;
-signal   mem_address_from_uut		: std_logic_vector (15 downto 0) := (others => '0');
-signal   mem_i_data_from_uut	: std_logic_vector (7 downto 0);
-signal   enable_wire_from_uut  		: std_logic;
-signal   mem_we_from_uut		        : std_logic;
-signal   mem_address_from_tb		: std_logic_vector (15 downto 0) := std_logic_vector(to_unsigned( 8 , 16));
-signal   mem_i_data_from_tb	: std_logic_vector (7 downto 0);
-signal   enable_wire_from_tb  		: std_logic := '0';
-signal   mem_we_from_tb		        : std_logic := '1';
 
-type ram_type is array (65535 downto 0) of std_logic_vector(7 downto 0);
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
 
-signal RAM: ram_type := (
-0 => std_logic_vector(to_unsigned(125 , 8)),
-1 => std_logic_vector(to_unsigned(55 , 8)),
-2 => std_logic_vector(to_unsigned(31 , 8)),
-3 => std_logic_vector(to_unsigned(37 , 8)),
-4 => std_logic_vector(to_unsigned(45 , 8)),
-5 => std_logic_vector(to_unsigned(77 , 8)),
-6 => std_logic_vector(to_unsigned(91 , 8)),
-7 => std_logic_vector(to_unsigned(100 , 8)),
-8 => std_logic_vector(to_unsigned(0 , 8)),
-others => (others =>'0'));
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
 
-component project_reti_logiche is
-port (
-      i_clk         : in  std_logic;
-      i_start       : in  std_logic;
-      i_rst         : in  std_logic;
-      i_data        : in  std_logic_vector(7 downto 0);
-      o_address     : out std_logic_vector(15 downto 0);
-      o_done        : out std_logic;
-      o_en          : out std_logic;
-      o_we          : out std_logic;
-      o_data        : out std_logic_vector (7 downto 0)
-      );
-end component project_reti_logiche;
+entity project_reti_logiche is
+    Port (
+        i_clk : in STD_LOGIC;
+        i_start : in STD_LOGIC;
+        i_rst : in STD_LOGIC;
+        i_data : in STD_LOGIC_VECTOR (7 downto 0);
+        o_address : out STD_LOGIC_VECTOR (15 downto 0);
+        o_done : out STD_LOGIC;
+        o_en : out STD_LOGIC;
+        o_we : out STD_LOGIC;
+        o_data : out STD_LOGIC_VECTOR (7 downto 0)
+    );
+end project_reti_logiche;
 
+architecture Behavioral of project_reti_logiche is
+
+component CU
+    Port (
+        clk, start, rst, loader_done: in STD_LOGIC;
+        done: out STD_LOGIC;
+        loader_wz_en: out STD_LOGIC;
+        loader_encode_en: out STD_LOGIC;
+        writer_en: out STD_LOGIC
+    );
+end component;
+
+component Loader
+    port(
+        clk, rst, loader_wz_en, loader_encode_en: in std_logic;
+		data_ram_in: in std_logic_vector(7 downto 0);
+		loader_done, driver_loader_en, reg_we: out std_logic;
+		addr_ram_from_loader: out std_logic_vector(3 downto 0);
+		addr_reg: out std_logic_vector(2 downto 0);
+		data_from_loader: out std_logic_vector(6 downto 0)
+    );
+end component;
+
+component Memory
+    port(
+        addr_reg: in std_logic_vector(2 downto 0);  
+        data_in: in std_logic_vector(6 downto 0);
+        clk, rst, we: in std_logic;
+        data0: out std_logic_vector(6 downto 0);
+        data1: out std_logic_vector(6 downto 0);
+        data2: out std_logic_vector(6 downto 0);
+        data3: out std_logic_vector(6 downto 0);
+        data4: out std_logic_vector(6 downto 0);
+        data5: out std_logic_vector(6 downto 0);
+        data6: out std_logic_vector(6 downto 0);
+        data7: out std_logic_vector(6 downto 0)
+    );
+end component;
+
+component Encoder
+    port(
+        wz0, wz1, wz2, wz3, wz4, wz5, wz6, wz7, to_be_coded: std_logic_vector(6 downto 0);
+        coded_result: out std_logic_vector(7 downto 0)
+    );
+end component;
+
+component Writer
+    port(
+        data_in: in std_logic_vector(7 downto 0); 
+        write_control: in std_logic; 
+        data_to_write: out std_logic_vector(7 downto 0); 
+        address_to_write: out std_logic_vector(3 downto 0); 
+        write_enable: out std_logic 
+        );
+end component;
+
+component Ram_driver
+    port(
+        addr_ram_from_loader: in STD_LOGIC_VECTOR (3 downto 0);
+        addr_ram_from_writer: in STD_LOGIC_VECTOR (3 downto 0);
+        data_ram_from_writer: in STD_LOGIC_VECTOR (7 downto 0);
+        driver_loader_en: in STD_LOGIC;
+        driver_writer_en: in STD_LOGIC;
+        addr_ram: out STD_LOGIC_VECTOR (15 downto 0);
+        data_ram_out: out STD_LOGIC_VECTOR (7 downto 0);
+        en_ram: out STD_LOGIC;
+        we_ram: out STD_LOGIC
+    );
+end component;
+
+signal loader_done: std_logic;
+signal loader_wz_en: std_logic;
+signal loader_encode_en: std_logic;
+signal writer_en: std_logic;
+signal driver_loader_en: std_logic;
+signal reg_we: std_logic;
+signal addr_ram_from_loader: std_logic_vector(3 downto 0);
+signal addr_reg:  std_logic_vector(2 downto 0);
+signal data_from_loader: std_logic_vector(6 downto 0);
+signal data0: std_logic_vector(6 downto 0);
+signal data1: std_logic_vector(6 downto 0);
+signal data2: std_logic_vector(6 downto 0);
+signal data3: std_logic_vector(6 downto 0);
+signal data4: std_logic_vector(6 downto 0);
+signal data5: std_logic_vector(6 downto 0);
+signal data6: std_logic_vector(6 downto 0);
+signal data7: std_logic_vector(6 downto 0);
+signal coded_result: std_logic_vector(7 downto 0);
+signal data_ram_from_writer: std_logic_vector(7 downto 0);
+signal addr_ram_from_writer: std_logic_vector(3 downto 0);
+signal driver_writer_en: std_logic;
 
 begin
-UUT: project_reti_logiche
-port map (
-          i_clk      	=> tb_clk,
-          i_start       => tb_start,
-          i_rst      	=> tb_rst,
-          i_data    	=> mem_o_data,
-          o_address  	=> mem_address_from_uut,
-          o_done      	=> tb_done,
-          o_en   	=> enable_wire_from_uut,
-          o_we 		=> mem_we_from_uut,
-          o_data    	=> mem_i_data_from_uut
-          );
-
-p_CLK_GEN : process is
-begin
-    wait for c_CLOCK_PERIOD/2;
-    tb_clk <= not tb_clk;
-end process p_CLK_GEN;
-
-
-MEM : process(tb_clk)
-begin
-    if tb_clk'event and tb_clk = '1' then
-        if enable_wire = '1' then
-            if mem_we = '1' then
-                RAM(conv_integer(mem_address))  <= mem_i_data;
-                mem_o_data                      <= mem_i_data after 1 ns;
-            else
-                mem_o_data <= RAM(conv_integer(mem_address)) after 1 ns;
-            end if;
-        end if;
-    end if;
-end process;
-
-
-ramdriver : process(mem_address_from_uut, mem_i_data_from_uut, enable_wire_from_uut, mem_we_from_uut, mem_address_from_tb, mem_i_data_from_tb, enable_wire_from_tb, mem_we_from_tb)
-begin
-    if enable_wire_from_tb = '1' then
-        mem_address <= mem_address_from_tb;
-        mem_i_data <= mem_i_data_from_tb;
-        enable_wire <= enable_wire_from_tb;
-        mem_we <= mem_we_from_tb;
-    else
-        mem_address <= mem_address_from_uut;
-        mem_i_data <= mem_i_data_from_uut;
-        enable_wire <= enable_wire_from_uut;
-        mem_we <= mem_we_from_uut;
-    end if;
-end process;
-
-
-test : process is
-begin 
-
-----------
-
---test section: da_ale
-wait for c_CLOCK_PERIOD;
-tb_rst <= '1';
-
---RAM gia' inizializzata
-
-wait for c_CLOCK_PERIOD;
-tb_rst <= '0';
-
---RAM gia' inizializzata
-
-tb_start <= '1';
-wait for c_CLOCK_PERIOD;
-wait until tb_done = '1';
-wait for c_CLOCK_PERIOD;
-tb_start <= '0';
-wait until tb_done = '0';
-
--- Maschera di output = 00000000
-assert RAM(9) = std_logic_vector(to_unsigned( 0 , 8)) report "***** TEST 1.1 FALLITO! ***** Expected 0 found " & integer'image(to_integer(unsigned(RAM(9))))  severity failure;
-report "TEST 1.1 PASSATO.";
-
-
-----------
+    cu_instance: CU port map(
+        clk=>i_clk, 
+        start=>i_start, 
+        rst=>i_rst, 
+        loader_done=>loader_done, 
+        done=>o_done, 
+        loader_wz_en=>loader_wz_en,
+        loader_encode_en=>loader_encode_en,
+        writer_en=>writer_en
+    );
     
-assert false report "Simulation Ended!" severity failure;
-end process test;
+    loader_instance: Loader port map(
+        clk=>i_clk, 
+        rst=>i_rst, 
+        loader_wz_en=>loader_wz_en,
+        loader_encode_en=>loader_encode_en,
+        data_ram_in=>i_data,
+        loader_done=>loader_done,
+        driver_loader_en=>driver_loader_en,
+        reg_we=>reg_we,
+        addr_ram_from_loader=>addr_ram_from_loader,
+        addr_reg=>addr_reg,
+        data_from_loader=>data_from_loader
+    );
+    
+    memory_instance: Memory port map(
+        addr_reg=>addr_reg,
+        data_in=>data_from_loader,
+        clk=>i_clk, 
+        rst=>i_rst, 
+        we=>reg_we,
+        data0=>data0,
+        data1=>data1,
+        data2=>data2,
+        data3=>data3,
+        data4=>data4,
+        data5=>data5,
+        data6=>data6,
+        data7=>data7
+    );
+    
+    encoder_instance: Encoder port map(
+        wz0=>data0,
+        wz1=>data1,
+        wz2=>data2,
+        wz3=>data3,
+        wz4=>data4,
+        wz5=>data5,
+        wz6=>data6,
+        wz7=>data7,
+        to_be_coded=>data_from_loader,
+        coded_result=>coded_result
+    );
+    
+    writer_instance: Writer port map(
+        data_in=>coded_result,
+        write_control=>writer_en,
+        data_to_write=>data_ram_from_writer,
+        address_to_write=>addr_ram_from_writer,
+        write_enable=>driver_writer_en
+    );
+    
+    ram_driver_instance: Ram_driver port map(
+        addr_ram_from_loader=>addr_ram_from_loader,
+        addr_ram_from_writer=>addr_ram_from_writer,
+        data_ram_from_writer=>data_ram_from_writer,
+        driver_loader_en=>driver_loader_en,
+        driver_writer_en=>driver_writer_en,
+        addr_ram=>o_address,
+        data_ram_out=>o_data,
+        en_ram=>o_en,
+        we_ram=>o_we
+    );
 
-end projecttb; 
+end Behavioral;
